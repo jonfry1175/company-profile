d5dc30ed0fd40365bcac0dfbf44df7f49f0cf28c
jonfry1175 <jonfrymarbun@gmail.com>
2025-02-23 11:39:05 +0700

    init all components

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <div className="home min-vh-100 w-100" id="home">
+      <Container>
+        <Row>
+          <Col className="text-center text-white fs-1">
+            <h1>Mari Bersantai Sejenak</h1>
+            <p className="text-white-50">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+              facere.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
